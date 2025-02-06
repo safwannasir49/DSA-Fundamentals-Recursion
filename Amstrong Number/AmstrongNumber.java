55c226479e7a57564d410251a4f20788ced2d72c
Safwan Nasir <127377148+safwannasir49@users.noreply.github.com>
2025-02-06 11:47:18 +0530

    Create AmstrongNumber.java

---
+++ Amstrong Number/AmstrongNumber.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            boolean flag = ob.armstrongNumber(n);
+            if (flag) {
+                System.out.println("true");
+
+            } else {
+                System.out.println("false");
+            }
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    static boolean armstrongNumber(int n) {
+        // code here
+        int digit , original=n;
+        int sum=0;
+        while(n!=0){
+        digit = n%10;
+        sum += Math.pow(digit,3);
+        n=n/10;
+        }
+        if(sum == original){
+            return true;
+        }
+        return false;
+    }
+}
