d3459269a2d50f3456bac57a7ed6875a94dbf962
Safwan Nasir <127377148+safwannasir49@users.noreply.github.com>
2025-05-20 16:52:07 +0530

    Update Sum of first N numbers using recursion.java

--- Sum of first N numbers using recursion/Sum of first N numbers using recursion.java
+++ Sum of first N numbers using recursion/Sum of first N numbers using recursion.java
@@ -1,33 +1,31 @@
-//{ Driver Code Starts
 import java.io.*;
 import java.util.*;
 
 class Main {
-    // Driver code
     public static void main(String[] args) throws Exception {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine().trim());
         while (t-- > 0) {
             int n = Integer.parseInt(br.readLine().trim());
             Solution obj = new Solution();
-            obj.printGfg(n);
-            System.out.println();
-        
-System.out.println("~");
-}
+            System.out.println(obj.NnumbersSum(n));
+            System.out.println("~");
+        }
     }
 }
-// } Driver Code Ends
-
 
 class Solution {
 
-    void printGfg(int N) {
-        // code here
-        for(int i=1;i<=N;i++)
+    public int NnumbersSum(int n) {
+
+        if(n==0) return 0;
+
+        int sum =0;
+        for(int i=1;i<=n;i++)
         {
-            System.out.print("GFG ");
+            sum += i;
         }
+
+        return sum;
     }
 }
